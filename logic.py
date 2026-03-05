7c66ab6ded24bddb2fc7a174b9e30742120a26d6
Ozgur3455 <ozgrygtydr@gmail.com>
2026-03-05 19:48:18 +0300

    Dövüşcü ve Büyücü eklendi

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 import aiohttp  # Eşzamansız HTTP istekleri için bir kütüphane
 import random
+import asyncio
 
 class Pokemon:
     pokemons = {}
@@ -8,11 +9,24 @@ class Pokemon:
         self.pokemon_trainer = pokemon_trainer
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
+        self.hp = random.randint(200,400)
+        self.power = random.randint(30,60)
         if pokemon_trainer not in Pokemon.pokemons:
             Pokemon.pokemons[pokemon_trainer] = self
         else:
             self = Pokemon.pokemons[pokemon_trainer]
-
+    async def attack(self, enemy):
+        if isinstance(enemy,Wizard):
+            chance = random.randint(1,5)
+            if chance == 1:
+                return "Sihirbaz Pokémon, savaşta bir kalkan kullanıldı!"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ne saldırdı\n@{enemy.pokemon_trainer}'nin sağlık durumu {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ni yendi!"
+    
     async def get_name(self):
         # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
@@ -28,7 +42,7 @@ class Pokemon:
         # Pokémon hakkında bilgi döndüren bir metot
         if not self.name:
             self.name = await self.get_name()  # Henüz yüklenmemişse bir adın geri alınması
-        return f"Pokémonunuzun ismi: {self.name}"  # Pokémon adını içeren dizeyi döndürür
+        return f"Pokémonunuzun ismi: {self.name},Sağlığı: {self.hp},Gücü: {self.power}"  # Pokémon adını içeren dizeyi döndürür
 
     async def show_img(self):
         # PokeAPI aracılığıyla bir pokémon görüntüsünün URL'sini almak için asenktron metot
@@ -40,7 +54,7 @@ class Pokemon:
                     return data['sprites']['front_default']  #  Pokémon adını döndürme
                 else:
                     return None # İstek başarısız olursa varsayılan adı döndürür
-    async def power(self):
+    async def ability(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
         async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
             async with session.get(url) as response:  # GET isteği gönderme
@@ -49,3 +63,25 @@ class Pokemon:
                     return data['abilities'][0]['ability']['name']  #  Pokémon adını döndürme
                 else:
                     return None  # İstek başarısız olursa varsayılan adı döndürür
+class Wizard(Pokemon):
+    pass
+class Fighter(Pokemon):
+    async def attack(self,enemy):
+        super_power = random.randint(5,15)
+        self.power += super_power
+        result = await super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nDövüşçü Pokémon süper saldırı kullandı. Eklenen güç: {super_power}"
+async def main():
+    wizard = Wizard("deniz")
+    fighter= Fighter("özgür")
+
+    print(await wizard.info())
+    print("#" * 10)
+    print(await fighter.info())
+    print("#" * 10)
+    print(await wizard.attack(fighter))
+    print(await fighter.attack(wizard))
+
+# Asenkron main fonksiyonunu çalıştırıyoruz
+asyncio.run(main())
